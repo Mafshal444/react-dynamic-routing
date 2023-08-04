2be54ab6849cd63f16138c0417c2d7c8c2fef5d8
Mafshal444 <112098761+Leomafshal@users.noreply.github.com>
2023-08-04 19:02:55 +0500

    implemented google-recaptcha both visible and invisible.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Link, Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+
 import User from './components/User';
 import Layout from './components/Layout';
+import { useRef, useState } from 'react';
 import Profile from './components/Profile';
 import Transactions from './components/Transactions';
 import Transaction from './components/Transaction';
-
+import ReCAPTCHA from "react-google-recaptcha";
 import './App.css';
 
 const App = () => {
@@ -14,9 +16,20 @@ const App = () => {
     { id: '1', details: 'Transaction 1' },
     { id: '2', details: 'Transactions 2' },
   ];
+  const [captchaToken, setCaptchaToken] = useState(null);
+  const captchaRef = useRef(null)
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    // const token = captchaRef.current.getValue();
+    const token = await captchaRef.current.executeAsync();
+    console.log("token values", token)
+    captchaRef.current.reset();
 
+}
   return (
     <div className='container'>
+      <ReCAPTCHA ref={captchaRef} sitekey={process.env.invisiblesiteKey} size="invisible"/>
+      <button onClick={handleSubmit} >submit </button>
       <Router>
         <h1>React Router</h1>
 
